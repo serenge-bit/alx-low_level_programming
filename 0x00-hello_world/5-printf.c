2bedbce1cb3cd0f6f5f5ac9891612096313183b0
serenge-bit <abigailserenge1@gmail.com>
2023-03-16 13:45:50 -0700

    wextra

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of execution and is returning an integer
+ *
+ * Return: to obey the void
+ */
+
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
