bd52bfbdbc3de7630780e2d3cb5cb056e3df4d3c
serenge-bit <abigailserenge1@gmail.com>
2023-03-16 16:03:18 -0700

    o qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *
+ * main - A C program that pints a line to the standard error
+ *
+ * Return: if 1 it is a success
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
