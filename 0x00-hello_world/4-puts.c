83dcbe217c640a77b0bf7c146c161152ccfe8827
serenge-bit <abigailserenge1@gmail.com>
2023-03-16 13:27:26 -0700

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: this is to make obey the void
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
