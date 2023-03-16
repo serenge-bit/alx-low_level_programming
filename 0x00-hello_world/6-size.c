e18590e0e3832883d99e444e94f2f659e35fe6ea
serenge-bit <abigailserenge1@gmail.com>
2023-03-16 14:36:34 -0700

    size of computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - return type of the main function
+ *
+ * Return: integer is returned
+ */
+
+int main(void)
+{	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of a int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}
