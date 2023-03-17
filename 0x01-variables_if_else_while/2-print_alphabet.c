33d5c0b3cb76fa325f3a4604d365fc922e3b0b7c
serenge-bit <abigailserenge1@gmail.com>
2023-03-17 12:57:07 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: o means success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar (i);
+	putchar('\n');
+
+	return (0);
+}
