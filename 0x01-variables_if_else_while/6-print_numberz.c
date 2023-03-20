7c9c421dcc799cd9af17224d71586f4bd3562858
serenge-bit <abigailserenge1@gmail.com>
2023-03-20 03:41:36 -0700

    putchar without using char for numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: success if 0
+ */
+
+int main(void)
+
+{	int i;
+
+	for (i = 0; i < 10; i++)
+	putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
