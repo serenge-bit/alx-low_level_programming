c7381402a8b121b7105f3a754076bd83a7660487
serenge-bit <abigailserenge1@gmail.com>
2023-03-20 14:58:58 -0700

    program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,13 +13,12 @@ int main(void)
 	for (d = 0; d < 10; d++)
 
 	{	putchar(d + '0');
-		if (d != 9)
+		if (d < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
+	putchar('\n');
 	return (0);
-
 }
