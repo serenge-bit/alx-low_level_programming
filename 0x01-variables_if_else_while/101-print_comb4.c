3d29f666bbe09f55324fcf8349619fac9526f9ee
serenge-bit <abigailserenge1@gmail.com>
2023-03-20 18:14:02 -0700

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,26 +15,23 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 1; j < 10; j++)
-
 		{
 			for (k = 2; k < 10; k++)
 			{
-				if (i < j && j < k)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-				}
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 				if (i == 7 && j == 8 && k == 9)
+				{
 					putchar('\n');
+				}
 				else
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}
+			}	
 		}
 	}
-
 	return (0);
 }
