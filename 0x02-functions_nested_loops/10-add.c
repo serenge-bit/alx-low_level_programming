8c8a11334241d778b79eea1aa4b13959298995a4
serenge-bit <abigailserenge1@gmail.com>
2023-03-21 14:53:41 -0700

    unction that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add -funtion name
+ *@i: function parameter
+ *@j: more function parameter
+ * Return: 0 if success
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - the funtion name
+ *
+ * Return: always 0
+ */
+void times_table(void)
+{
+
+}
