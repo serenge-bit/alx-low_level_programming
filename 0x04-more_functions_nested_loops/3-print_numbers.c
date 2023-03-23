595b6538bfddacae291733f6a1aff48a5a70c0d0
serenge-bit <abigailserenge1@gmail.com>
2023-03-23 08:03:46 -0700

    putchat

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - funtion name
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+	{
+		-putchar(i + '0');
+	}
+	_putchar('\n');
+}
